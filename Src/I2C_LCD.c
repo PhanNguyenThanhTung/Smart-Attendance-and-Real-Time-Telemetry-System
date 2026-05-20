96976db8b3981fe2ab8b226030ee2125b2fa09a9
Thanh Tung <156272078+PhanNguyenThanhTung@users.noreply.github.com>
2026-05-21 02:05:00 +0700

    update

--- Src/I2C_LCD.c
+++ Src/I2C_LCD.c
@@ -18,5 +18,52 @@ void I2C_Init(void) {
 }
 
 uint8_t I2C_WriteByte(uint8_t addr, uint8_t data) {
-    
+    uint32_t timeout;
+    timeout = 100000;
+    //doi bus het ban, Status_Register_2 bit 1 la Bus Busy, 1 la co 0 la ko
+    while((I2C1_SR2 & (1 << 1)) && timeout--);
+    if(timeout == 0) {
+        return 0;
+    }
+    //Bat dau I2C
+    I2C1_CR1 |= (1 << 8); //bit 8 trong CR1 la Start
+    //Doi SB (Start Bit) thanh ghi SR1
+    timeout = 100000;
+    while(!(I2C1_SR1 & (1 << 0)) && timeout--);
+    if(timeout == 0) {
+        return 0;
+    }
+    //Gui dia chi Slave + Write bit 
+    I2C1_DR = (addr << 1); //Dia chi gui di gom (A6 A5 A4 A3 A2 A1 | Write/Read bit) 0 la Write, 1 la Read
+    //Doi Address
+    timeout = 100000;
+    while(!(I2C1_SR1 & (1 << 1)) && timeout--); //o che do Master bit 1 thanh ghi SR1 neu 0 la dang truyen dia chi qua cho Slave con 1 la da truyen xong
+    if(timeout == 0) {
+        I2C1_CR1 |= (1 << 9); //bat Stop bit
+        return 0;
+    }
+    //Doc theo thu tu cua 2 thanh ghi de clear co ADDR
+    (void)I2C1_SR1;
+    (void)I2C1_SR2;
+    //Doi co TXE: Transmit Emty 
+    timeout = 100000;
+    while (!(I2C1_SR1 & (1 << 7)) && timeout--);
+    if(timeout == 0) {
+        I2C1_CR1 |= (1 << 9); //bat Stop Bit
+        return 0;
+    }
+    //Gui data
+    I2C1_DR = data;
+    //Doi BTF: Byte transfer finished, 0: chua xong | 1: da xong
+    timeout = 100000;
+    while(!(I2C1_SR1 & (1 << 2)) && timeout--);
+    if(timeout == 0) {
+        I2C1_CR1 |= (1 << 9);
+        return 0;
+    }
+    //Stop
+    I2C1_CR1 |= (1 << 9);
+
+    return 1;
 }
+
